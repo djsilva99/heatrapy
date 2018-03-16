3661ab85e1820523b59a8c2ea0e04171f92eb9c2
Unknown <djsilva99@gmail.com>
2018-03-16 10:55:59 +0000

    Version v0.2.5

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ description += 'systems.'
 
 
 setup(name='heatrapy',
-      version='0.2.4',
+      version='0.2.5',
       description='Library for simulating heat transfer processes',
       long_description=description,
       classifiers=[
