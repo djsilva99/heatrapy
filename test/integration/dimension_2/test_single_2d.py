6486eefd066bb8021a5992847e604534ac4cf151
Daniel Silva <djsilva@gmx.com>
2024-09-15 20:36:58 +0100

    test: increase coverage (#25)
    * test: increase coverage
    
    * test: add more scenarios for single 2D
    
    * test: add more scenarios for single 2D
    
    * test: add more scenarios for single 1D
    
    * test: add more scenarios for single 2D
    
    * chore: change target to 70%

--- test/integration/dimension_2/test_single_2d.py
+++ test/integration/dimension_2/test_single_2d.py
@@ -4,29 +4,58 @@ from heatrapy.dimension_2.objects import SingleObject as SingleObject2D
 
 @pytest.fixture
 def example_single_object_2d():
-    return SingleObject2D(300, boundaries=(200, 0, 0, 0), draw=[])
+    return SingleObject2D(
+        300,
+        boundaries=(200, 0, 0, 0),
+        draw=[
+            "materials", "temperature", "state", "Q", "Q0"
+        ]
+    )
+
+
+@pytest.fixture
+def example_single_object_2d_activated():
+    return SingleObject2D(
+        300,
+        boundaries=(200, 0, 0, 0),
+        draw=[
+            "materials", "temperature", "state", "Q", "Q0"
+        ],
+        initial_state=True
+    )
 
 
 def test_explicit_general_single_2d(example_single_object_2d):
     solution = 245
     example_single_object_2d.compute(30, 5, solver='explicit_general')
+    example_single_object_2d.show_figure("temperature")
+    example_single_object_2d.show_figure("materials")
+    example_single_object_2d.show_figure("state")
+    example_single_object_2d.show_figure("Q")
+    example_single_object_2d.show_figure("Q0")
     assert int(
         example_single_object_2d.object.temperature[5][5][0]
     ) == solution
 
 
-def test_explicit_k_single_2d(example_single_object_2d):
+def test_explicit_k_single_2d(example_single_object_2d_activated):
     solution = 245
-    example_single_object_2d.compute(30, 5, solver='explicit_k(x)')
+    example_single_object_2d_activated.activate((4, 4), 2, shape="circle")
+    example_single_object_2d_activated.deactivate((4, 4), 2, shape="circle")
+    example_single_object_2d_activated.activate((2, 2), (3, 3))
+    example_single_object_2d_activated.deactivate((2, 2), (3, 3))
+    example_single_object_2d_activated.compute(30, 5, solver='explicit_k(x)')
+
     assert int(
-        example_single_object_2d.object.temperature[5][5][0]
+        example_single_object_2d_activated.object.temperature[5][5][0]
     ) == solution
 
 
 def test_explicit_k_single_2d_with_activation(example_single_object_2d):
     solution = 245
+    example_single_object_2d.activate((4, 4), 2, shape="circle")
+    example_single_object_2d.deactivate((4, 4), 2, shape="circle")
     example_single_object_2d.activate((2, 2), (3, 3))
-    example_single_object_2d.deactivate((2, 2), (3, 3))
     example_single_object_2d.compute(30, 5, solver='explicit_k(x)')
     assert int(
         example_single_object_2d.object.temperature[5][5][0]
@@ -45,6 +74,7 @@ def test_explicit_k_single_2d_with_bondary_change(example_single_object_2d):
 def test_explicit_k_single_2d_with_material_change(example_single_object_2d):
     solution = 245
     example_single_object_2d.change_material("square", "Cu", (1, 1), (2, 2))
+    example_single_object_2d.change_material("circle", "Cu", (4, 4), 2)
     example_single_object_2d.compute(30, 5, solver='explicit_k(x)')
     assert int(
         example_single_object_2d.object.temperature[5][5][0]
@@ -54,7 +84,16 @@ def test_explicit_k_single_2d_with_material_change(example_single_object_2d):
 def test_explicit_k_single_2d_with_power_change(example_single_object_2d):
     solution = 245
     example_single_object_2d.change_power(
+        "square", "Q", (1, 1), (2, 2), 0
+    )
+    example_single_object_2d.change_power(
+        "circle", "Q", (4, 4), 2, 0
+    )
+    example_single_object_2d.change_power(
         "square", "Q0", (1, 1), (2, 2), 0
+    )
+    example_single_object_2d.change_power(
+        "circle", "Q0", (4, 4), 2, 0
     )
     example_single_object_2d.compute(30, 5, solver='explicit_k(x)')
     assert int(
