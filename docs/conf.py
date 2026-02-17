7961cbe910aa4a34900d31b16bac230ba428bb98
Daniel Silva <djsilva@gmx.com>
2026-02-17 22:26:34 +0000

    chore: prepare patch release

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = 'heatrapy'
 copyright = '2024, Daniel Silva'
 author = 'Daniel Silva'
-release = '2.0.12'
+release = '2.0.13'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
