b0258cf97c36839b646c9dfb26ef2267c53bf329
Daniel Silva <djsilva@gmx.com>
2026-02-17 22:29:14 +0000

    chore: upagrade numpy (#67)
    * chore: upagrade numpy
    
    * chore: prepare patch release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ description += 'package includes the incorporation of phase transitions.'
 
 
 setup(name='heatrapy',
-      version='2.0.12',
+      version='2.0.13',
       description='Library for simulating 1D and 2D heat transfer processes',
       long_description=description,
       classifiers=[
